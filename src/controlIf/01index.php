2ab1d34a22c9a262797335be37ba07d4ff3de5ae
永田圭 <nagatakei@nagatakeinoMacBook-Air.local>
2021-04-18 11:55:59 +0900

    controlIf

--- src/controlIf/01index.php
+++ src/controlIf/01index.php
@@ -1,5 +1,20 @@
 <?php
 
+echo "数字を入力してください";
+$num = trim(fgets(STDIN));
+
+if ($num >10){
+    echo '10よりも大きいです';
+} elseif ($num <= 10){
+    echo '10よりも小さいです';
+}
+
+// if ($num >10):
+//     echo '10よりも大きいです';
+// elseif ($num <= 10):
+//     echo '10よりも小さいです';
+// endif
+
 
 
 
---
+++ src/controlIf/03index.php
@@ -0,0 +1,13 @@
+<?php
+
+$number = 10;
+$numeric = is_numeric($number);
+
+if($numeric){
+    echo "数字です";
+} else {
+    echo "数字ではありません";
+}
+
+
+?>
