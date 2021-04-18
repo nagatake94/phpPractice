7117001d2402b45f4a942b59b3ea814b27654171
永田圭 <nagatakei@nagatakeinoMacBook-Air.local>
2021-04-18 10:54:44 +0900

    testPHP

---
+++ src/test/02index.php
@@ -0,0 +1,12 @@
+<?php
+
+//echo "I'm Japanese.";
+
+echo 'I\'m Japanese.';
+echo "I'm \"Japanese\".";
+
+echo "\n改行を\n入れて出力します";
+echo '\n改行を\n入れて出力します'
+
+
+?>
---
+++ src/test/06index.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "数字を入力してください。";
+//$num= fgets(STDIN);
+$num = trim(fgets(STDIN)); 
+echo $num, 'ですね？';
+
+?>
