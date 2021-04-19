c56d90ec15dfd6d682417014db23fca79986d632
永田圭 <nagatakei@nagatakeinoMacBook-Air.local>
2021-04-20 07:21:21 +0900

    controlFunc

---
+++ src/controlFunc/02index.php
@@ -0,0 +1,12 @@
+<?php
+
+//足し算をする関数
+function sum($num1, $num2){
+    $answer = $num1 + $num2;
+
+    return $answer;
+}
+
+$item_sum = sum(167, 200);
+echo $item_sum;
+
---
+++ src/controlFunc/08index.php
@@ -0,0 +1,27 @@
+<?php
+
+//require('09index.php');
+include'09index.php';
+
+echo sum(10,15);
+
+//数字を成形して表示する
+// function echo_price($callback){
+//     echo number_format($callback(1000,500)), '円';
+// }
+
+// $get_sum = function($a, $b){
+//     return $a+$b;
+// };
+
+
+// $sum = $get_sum(10,5);
+// echo $sum;
+
+//echo_price($get_sum);
+
+// echo_price(function($a, $b){
+//     return $a+$b;
+// });
+
+//$fn1 = fn($x) => $x + $y;
